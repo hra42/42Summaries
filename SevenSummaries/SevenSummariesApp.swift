98321ca229688abccfefef5929e538efb6fd6a1f
Henry Rausch <44985923+hra42@users.noreply.github.com>
2024-10-04 00:52:04 +0200

    Initial Commit

---
+++ SevenSummaries/SevenSummariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenSummariesApp.swift
+//  SevenSummaries
+//
+//  Created by Henry Rausch on 04.10.24.
+//
+
+import SwiftUI
+
+@main
+struct SevenSummariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
